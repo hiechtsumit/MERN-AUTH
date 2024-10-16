696d2625a826c9fe8cfcc53affcfa50bdba2b340
DanaDonate <smondalonly@gmail.com>
2024-10-16 13:28:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -31,5 +31,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(PORT, () => {
 	connectDB();
-	console.log("Server is running on port: ", PORT);
+	console.log("Server is running on the port: ", PORT);
 });
